0f29ba3ae4bef4cf555a98ce5b13852cddb5e27a
devMukulSingh <mukulsingh2276@gmail.com>
2024-07-05 14:42:39 +0530

    first commit

---
+++ app/(root)/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="fixed flex items-center shadow-lg justify-center top-0 w-screen bg-[#dff9fb] h-20">
+      <h1 className="font-medium  text-4xl">Image Formatter</h1>
+    </div>
+  );
+}
+
+export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter,Bona_Nova } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Bona_Nova({ subsets: ["latin"], weight:'400' });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        <Toaster/>
+        {children}
+        </body>
     </html>
   );
 }
